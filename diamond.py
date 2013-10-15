7edcf66a601b1071e8b57a625c7eb3813a2fd8fd
Stefan Ellmauthaler <ellmauthaler@informatik.uni-leipzig.de>
2013-10-15 16:50:05 +0200

    version 0.9

--- diamond.py
+++ diamond.py
@@ -1,11 +1,90 @@
 #!/usr/bin/env python
+################################################################################
+##
+## Copyright 2013 Stefan Ellmauthaler, ellmauthaler@informatik.uni-leipzig.de
+##
+## This file is part of diamond.
+##
+## diamond is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## diamond is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with diamond.  If not, see <http://www.gnu.org/licenses/>.
+##
+################################################################################
+##
+## diamond.py
+##
 import argparse
+import configparser as cp
 import os
 import tempfile
 import time
 import sys
 
-version='0.8'
+version='0.9'
+
+# default variables
+encdir = "lib"
+installdir = os.path.dirname(os.path.realpath(__file__))
+gringo = "gringo"
+gringo305 = "gringo"
+clasp = "clasp"
+claspD = "claspD"
+eclipse = "eclipse"
+
+# encoding filenames
+enc = dict(
+    tkk = "3KK.lp ",
+    adm = "admissible.lp ",
+    base = "base.lp ",
+    cf = "cf.lp ",
+    cmp = "complete.lp ",
+    grd = "grounded.lp ",
+    model = "model.lp ",
+    op = "op.lp ",
+    opsm = "opsm.lp ",
+    prf = "preferred.lp ",
+    prio_trans = "prio_trans.lp ",
+    stb = "stable.lp ",
+    transformpl = "transform.pl ",
+    transformpy = "transform.py ")
+
+
+def getoptval(config,section,option,default):
+    if config.has_option(section,option):
+        return config.get(section,option)
+    else:
+        return default
+
+def initvars(cfgfile):
+    global installdir, gringo, gringo305,clasp,claspD,eclipse
+    cfgfile = os.path.expandvars(os.path.expanduser(cfgfile))
+    config = cp.ConfigParser()
+    if os.path.exists(cfgfile):
+        config.read_file(open(cfgfile))
+        installdir = getoptval(config,"Path","installdir",installdir)
+        gringo = getoptval(config,"Path","gringo",gringo)
+        gringo305 = getoptval(config,"Path","gringo305",gringo305)
+        clasp = getoptval(config,"Path","clasp",clasp)
+        claspD = getoptval(config,"Path","claspD",claspD)
+        eclipse = getoptval(config,"Path","eclipse",eclipse)
+    else: #config file does not exist - create one
+        config.add_section("Path")
+        config.set("Path","installdir",installdir)
+        config.set("Path","gringo", gringo)
+        config.set("Path","gringo305", gringo)
+        config.set("Path","clasp", clasp)
+        config.set("Path","claspD", claspD)
+        config.set("Path","eclipse", eclipse)
+        config.write(open(cfgfile,'w'))
 
 def main():
     parser= argparse.ArgumentParser(description='Program to compute different models and sets for a given ADF')
@@ -17,6 +96,7 @@ def main():
     parser.add_argument('-c', '--complete', help='compute the complete models', action='store_true', dest='complete')
     parser.add_argument('-a', '--admissible', help='compute the admissible models', action='store_true', dest='admissible')
     parser.add_argument('-p', '--preferred', help='compute the preferred model', action='store_true', dest='preferred')
+    parser.add_argument('-cfg', help='specify a config-file', action='store', dest='cfgfile', default='~/.diamond')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--transform_pform', help='transform a pForm ADF before the computation', action='store_true', dest='transformpform')
     group.add_argument('--transform_prio', help='transform a prioritized ADF before the computation', action='store_true', dest='transformprio')
@@ -26,6 +106,10 @@ def main():
     args=parser.parse_args()
     tmp=tempfile.NamedTemporaryFile(delete=True)
     instance=os.path.abspath(args.instance)
+    claspstring = " 2> /dev/null | " + clasp + " 0 2> /dev/null"
+    initvars(args.cfgfile)
+    for el in iter(enc):
+        enc[el] = installdir + "/" + encdir + "/" + enc[el]
     if args.version:
         print("==============================")
         print("DIAMOND version " + version)
@@ -36,7 +120,8 @@ def main():
         print("==============================")
         print("transforming pForm ADF...")
         #start = time.time()
-        os.system("./transform.sh " + os.path.abspath(args.instance) + " " + instance)
+        os.system(eclipse + " -b " + enc['transformpl'] + "-e main -- " + os.path.abspath(args.instance) + " " + instance)
+        #os.system("./transform.sh " + os.path.abspath(args.instance) + " " + instance)
         #os.system("python transform.py --propositional " + os.path.abspath(args.instance) + " > " + instance)
         #elapsed = (time.time() - start)
         #elapsedstring = "%.3f" % (elapsed,)
@@ -48,7 +133,10 @@ def main():
         print("==============================")
         print("transforming prioritized ADF...")
         start = time.time()
-        os.system("python transform.py --prioritized " + os.path.abspath(args.instance) + " > " + instance)
+        wd = os.getcwd()
+        os.chdir(installdir + "/" + encdir)
+        os.system("python " + enc['transformpy'] + os.path.abspath(args.instance) + " > " + instance)
+        os.chdir(wd)
         elapsed = (time.time() - start)
         elapsedstring = "%.3f" % (elapsed,)
         print("transformation took " + elapsedstring  + " seconds")    
@@ -56,38 +144,37 @@ def main():
         print("==============================")
         print("conflict free sets:")
         os.system("echo '#hide.#show in/1.' > " + tmp.name)
-        os.system("gringo base.lp cf.lp " + instance + " " + tmp.name + " | clasp 0 2> /dev/null")
+        os.system(gringo + " " + enc['base'] + enc['cf'] + instnace + " " + tmp.name + claspstring) 
     if args.model or args.all:
         print("==============================")
         print("two-valued models")
         os.system("echo '#hide.#show in/1.' > " + tmp.name)
-        os.system("gringo base.lp cf.lp model.lp " + instance + " " + tmp.name + " | clasp 0 2> /dev/null")
+        os.system(gringo + " " + enc['base'] + enc['cf'] + enc['model'] + instance + " " + tmp.name + claspstring)
     if args.smodel or args.all:
         print("==============================")
         print("stable models:")
         os.system("echo '#hide.#show in/1.#show out/1.' > " + tmp.name)
-        os.system("gringo base.lp cf.lp model.lp opsm.lp 3KK.lp stable.lp " + instance + " " + tmp.name + " | clasp 0 2> /dev/null")
+        os.system(gringo + " " + enc['base'] + enc['cf'] + enc['model'] + enc['opsm'] + enc['tkk'] + enc['stb'] + instance + " " + tmp.name + claspstring)
     if args.grounded or args.all:
         print("==============================")
         print("grounded model")
         os.system("echo '#hide.#show in/1.#show out/1.#show udec/1.' > " + tmp.name)
-        os.system("gringo base.lp op.lp 3KK.lp grounded.lp " + instance + " " + tmp.name + " | clasp 0 2> /dev/null")
+        os.system(gringo + " " + enc['base'] + enc['op'] + enc['tkk'] + enc['grd'] + instance + " " + tmp.name + claspstring)
     if args.complete or args.all:
         print("==============================")
         print("complete models:")
         os.system("echo '#hide.#show in/1.#show out/1.#show udec/1.' > " + tmp.name)
-        os.system("gringo base.lp op.lp complete.lp " + instance + " " + tmp.name + " | clasp 0 2> /dev/null")
+        os.system(gringo + " " + enc['base'] + enc['op'] + enc['cmp'] + instance + " " + claspstring)
     if args.admissible or args.all:
         print("==============================")
         print("admissible models:")
         os.system("echo '#hide.#show in/1.#show out/1.#show udec/1.' > " + tmp.name)
-        os.system("gringo base.lp op.lp admissible.lp " + instance + " " + tmp.name + " | clasp 0 2> /dev/null")
+        os.system(gringo + " " + enc['base'] + enc['op'] + enc['adm'] + instance + " " + tmp.name + claspstring)
     if args.preferred:# or args.all:
         print("==============================")
         print("preferred model:")
         os.system("echo 'optimize(1,1,incl).' > " + tmp.name) 
-        os.system('gringo_305 --reify admissible.lp preferred.lp op.lp base.lp ' + instance + ' | gringo_305 - metasp/{meta.lp,metaD.lp,metaO.lp} ' + tmp.name + ' | claspD 0')
-
+        os.system(gringo305 + " --reify " + enc['adm'] + enc['prf'] + enc['op'] + enc['base'] + instance + " 2> /dev/null | " + gringo305 + " - " + installdir + "/" + encdir + "/metasp/{meta.lp,metaD.lp,metaO.lp} " + tmp.name + " 2> /dev/null | " + claspD + " 0 ")
 
 if __name__ == "__main__":
     main()
